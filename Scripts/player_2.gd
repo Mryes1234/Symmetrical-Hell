bd6204725dbc9aef79e0f646a54aae13317b3c56
Aidyn Jackson <ajackson6825@student.pps.net>
2025-12-05 15:33:34 -0800

    summary 2, got collision working!

--- Scripts/player_2.gd
+++ Scripts/player_2.gd
@@ -4,6 +4,8 @@ var xdirection = 0
 var xspeed = 100
 var ydirection = 0
 var yspeed = 100
+var max_health: int = 10
+var health: int = max_health
 
 func _physics_process(delta: float) -> void:
 	if xdirection:
