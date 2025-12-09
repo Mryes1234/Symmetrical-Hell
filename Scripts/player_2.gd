d9c5e062c3ecb42040c20deddc498c9cbf02f21d
Aidyn Jackson <ajackson6825@student.pps.net>
2025-12-09 15:32:35 -0800

    Summary 3, trying damage collision

--- Scripts/player_2.gd
+++ Scripts/player_2.gd
@@ -6,8 +6,47 @@ var ydirection = 0
 var yspeed = 100
 var max_health: int = 10
 var health: int = max_health
+var damage_amount: int = 1
 
-func _physics_process(delta: float) -> void:
+signal health_changed(new_health: float, max_health: float)
+signal player_died
+
+func _ready():
+	health = max_health
+	collision_layer = 1
+
+func _on_Area2D_area_entered(area):
+	if area.is_in_group("Player"):
+		print("Something")
+		area.apply_damage(damage_amount)
+		print("Something4")
+
+func take_damage(amount: float) -> bool:
+	health -= amount
+	print("Something1")
+	health = max(0, health)
+	print("Something2")
+	health_changed.emit(health, max_health)
+	print("Something3")
+
+	if health <= 0:
+		die()
+		print("Something5")
+		return true
+
+	return false
+
+func die() -> bool:
+	print("you died!")
+	player_died.emit()
+	# Disable player controls
+	set_physics_process(false)
+	# Hide or play death animation
+	visible = false
+
+	return true
+
+func _physics_process(delta):
 	if xdirection:
 		velocity.x = xdirection * xspeed
 	else:
